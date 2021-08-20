3ab95d4cb807dc225584311e1e9e79d027d4f44d
pseudoyu <pseudoyu@connect.hku.hk>
2021-08-20 13:52:51 +0800

    feat: add github action automation

--- README.md
+++ README.md
@@ -34,7 +34,7 @@
 <div>
 <p>
 
-区块链（Golang）开发工程师，香港大学计算机系硕士在读。喜欢探索学习新科技，空闲时也折腾Notion等效率工具。
+区块链（Golang）开发工程师，香港大学计算机系硕士在读。喜欢探索学习新科技，空闲时也折腾 Notion 等效率工具。
 
 </h4>
 </div>
@@ -48,3 +48,38 @@
 ![Yu Zhang's github stats](https://yu-readme.vercel.app/api?username=pseudoyu&show_icons=true)
 
 [![Top Langs](https://yu-readme.vercel.app/api/top-langs/?username=pseudoyu&layout=compact&hide=javascript,html,php,css,python,TypeScript,Smarty)](https://github.com/pseudoyu)
+
+#### 🎣 <a href="https://www.pseudoyu.com/zh/" target="_blank">Recent Blog</a>
+
+<!-- blog starts -->
+* <a href=https://tw93.github.io/2020-05-27/good-app.html target='_blank'>我的 2020 🔥软件【分享】</a> - 2020-05-27
+* <a href=https://tw93.github.io/2020-04-21/one.html target='_blank'>飞猪统一运营工作台的解决方案</a> - 2020-04-21
+* <a href=https://tw93.github.io/2020-03-30/serverless-two.html target='_blank'>飞猪 Serverless 体系建设（S2）</a> - 2020-03-30
+* <a href=https://tw93.github.io/2020-03-18/how-to-do-design.html target='_blank'>运营后台的交互设计【分享】</a> - 2020-03-18
+* <a href=https://tw93.github.io/2020-03-15/serverless-one.html target='_blank'>飞猪 Serverless 体系建设（S1）</a> - 2020-03-15
+<!-- blog ends -->
+
+#### 🏊‍♂️ <a href="https://gist.github.com/pseudoyu/8f303370b8c15ccd49ed42f1e5df2404" target="_blank">Weekly Development Breakdown</a>
+
+<!-- code_time starts -->
+
+```text
+Markdown     2 hrs 20 mins  ██████████░░░░░░░░░░  36.4%
+Python             49 mins  █████▒░░░░░░░░░░░░░░  12.8%
+Swift              43 mins  █████░░░░░░░░░░░░░░░  11.4%
+JSX                33 mins  ████▓░░░░░░░░░░░░░░░   8.8%
+TypeScript         32 mins  ████▓░░░░░░░░░░░░░░░   8.4%
+JavaScript         24 mins  ████░░░░░░░░░░░░░░░░   6.3%
+```
+
+<!-- code_time ends -->
+
+#### 🏋️‍♀️ <a href="https://github.com/pseusoyu/pseudoyu/blob/main/releases.md" target="_blank">Product Releases</a>
+
+<!-- recent_releases starts -->
+* <a href=https://github.com/tw93/tw93.github.io/releases/tag/V0.1 target='_blank'>tw93.github.io 修复RSS报错</a> - 2020-07-18
+* <a href=https://github.com/tw93/cz-emoji-chinese/releases/tag/V0.2.1 target='_blank'>cz-emoji-chinese 完善使用文档</a> - 2020-07-18
+* <a href=https://github.com/tw93/tw93/releases/tag/V0.1 target='_blank'>tw93 生成首页信息流</a> - 2020-07-18
+* <a href=https://github.com/tw93/MiaoYan/releases/tag/V0.1 target='_blank'>MiaoYan 第一个可用版本</a> - 2020-05-28
+* <a href=https://github.com/alibaba/form-render/releases/tag/v0.3.1 target='_blank'>form-render 支持函数表达式</a> - 2019-11-22
+<!-- recent_releases ends -->
---
+++ build_readme.py
@@ -0,0 +1,160 @@
+from python_graphql_client import GraphqlClient
+import feedparser
+import httpx
+import json
+import pathlib
+import re
+import os
+import datetime
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+TOKEN = os.environ.get("GH_TOKEN", "")
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def formatGMTime(timestamp):
+    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+    return dateStr.date()
+
+def make_query(after_cursor=None):
+    return """
+query {
+  viewer {
+    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        name
+        description
+        url
+        releases(last:1) {
+          totalCount
+          nodes {
+            name
+            publishedAt
+            url
+          }
+        }
+      }
+    }
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+def fetch_releases(oauth_token):
+    repos = []
+    releases = []
+    repo_names = set()
+    has_next_page = True
+    after_cursor = None
+
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+        print()
+        print(json.dumps(data, indent=4))
+        print()
+        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
+                repos.append(repo)
+                repo_names.add(repo["name"])
+                releases.append(
+                    {
+                        "repo": repo["name"],
+                        "repo_url": repo["url"],
+                        "description": repo["description"],
+                        "release": repo["releases"]["nodes"][0]["name"]
+                        .replace(repo["name"], "")
+                        .strip(),
+                        "published_at": repo["releases"]["nodes"][0][
+                            "publishedAt"
+                        ].split("T")[0],
+                        "url": repo["releases"]["nodes"][0]["url"],
+                    }
+                )
+        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
+            "hasNextPage"
+        ]
+        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
+    return releases
+
+
+def fetch_code_time():
+    return httpx.get(
+        "https://gist.githubusercontent.com/pseudoyu/cc924377b8354a9f1738d184d4951c45/raw/"
+    )
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://www.pseudoyu.com/zh/index.xml")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    project_releases = root / "releases.md"
+    releases = fetch_releases(TOKEN)
+    releases.sort(key=lambda r: r["published_at"], reverse=True)
+    md = "\n".join(
+        [
+            "* <a href={url} target='_blank'>{repo} {release}</a> - {published_at}".format(**release)
+            for release in releases[:8]
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+
+    # Write out full project-releases.md file
+    project_releases_md = "\n".join(
+        [
+            (
+                "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_at}\n"
+                "<br>{description}"
+            ).format(**release)
+            for release in releases
+        ]
+    )
+    project_releases_content = project_releases.open().read()
+    project_releases_content = replace_chunk(
+        project_releases_content, "recent_releases", project_releases_md
+    )
+    project_releases_content = replace_chunk(
+        project_releases_content, "release_count", str(len(releases)), inline=True
+    )
+    project_releases.open("w").write(project_releases_content)
+
+    code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
+
+    rewritten = replace_chunk(rewritten, "code_time", code_time_text)
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* <a href={url} target='_blank'>{title}</a> - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+
+    readme.open("w").write(rewritten)
