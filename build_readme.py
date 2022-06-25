29785e8e31b237e951e80653f7507a81ef32945b
pseudoyu <pseudoyu@connect.hku.hk>
2022-06-26 02:29:13 +0800

    fix: time format

--- build_readme.py
+++ build_readme.py
@@ -172,9 +172,7 @@ if __name__ == "__main__":
 
     entries = fetch_blog_entries()[:5]
     entries_md = "\n".join(
-        ["* <a href='{url}' target='_blank'>{title}</a> - {published}".format(**entry) for entry in entries]
-        # ["* <a href={url} target='_blank'>{title}</a> - {published}".format(**entry) for entry in entries]
-        # ["* <a href={url} target='_blank'>{title}</a>".format(**entry) for entry in entries]
+        ["* <a href={url} target='_blank'>{title}</a>".format(**entry) for entry in entries]
     )
     rewritten = replace_chunk(rewritten, "blog", entries_md)
 
